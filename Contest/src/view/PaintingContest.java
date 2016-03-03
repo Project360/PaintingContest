b9b55eae9646c83a0afed2c63b563cdfd58265df
Sara Vandandaigue <project360uw@gmail.com>
2016-03-03 10:13:43 -0800

    Added the blank Model objects: Contestant, Entry, Judge, Library. Added the blank view objects: DownloadScreen, EntryScreen, JudgingScreen

--- Contest/src/view/PaintingContest.java
+++ Contest/src/view/PaintingContest.java
@@ -1,10 +1,42 @@
 package view;
 
+import java.util.HashMap;
+
+import model.Entry;
+
+/**
+ * The PaintingContest object represents the coloring contest. It contains the
+ * submitted contest entries which is loaded at the start of the program from the
+ * attached textfile. It starts by displaying the Registration screen and changes
+ * the GUI based on the user actions.
+ * 
+ * @author Sara Vandandaigue
+ * @version 0.1
+ *
+ */
 public class PaintingContest {
+	
+	/**
+	 * Map of entryID to Entry object.
+	 */
+	private HashMap<Integer, Entry> myContestEntries;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Registration();
 	}
+	
+	/**
+	 * Loads the contest entries from the textfile (DB).
+	 * @return
+	 */
+	private boolean loadEntries() {
+		//load entries from textfile
+		return true;
+	}
+	
+	public HashMap<Integer, Entry> getEntries() {
+		return myContestEntries;
+	}
+	
 
 }
